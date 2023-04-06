362454ec640838d8a59e95c9e4fa374006722131
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-04-06 11:37:03 +0300

    add params in notice schema

--- backend/models/notices.js
+++ backend/models/notices.js
@@ -1,36 +1,54 @@
-const { Schema, model, SchemaTypes } = require('mongoose');
+const { Schema, model, SchemaTypes } = require("mongoose");
 
 const noticesSchema = new Schema(
   {
-    owner: { type: SchemaTypes.ObjectId, ref: 'users' },
+    owner: { type: SchemaTypes.ObjectId, ref: "users" },
     category: {
       type: String,
-      enum: ['sell', 'lost-found', 'for-free'],
-      required: [true, 'Category is required'],
+      enum: ["sell", "lost-found", "for-free", "none"],
+      required: [true, "Category is required"],
+    },
+    typeofpet: {
+      type: String,
+      enum: ["cat", "dog"],
+      required: [true, "Type of pet is required"],
     },
     title: {
       type: String,
-      required: [true, 'Title is required'],
+      required: [true, "Title is required"],
     },
     name: {
       type: String,
-      required: [true, 'Name is required'],
+      required: [true, "Name is required"],
     },
     birthday: {
       type: Date,
     },
     breed: {
       type: String,
-      required: [true, 'Breed is required'],
+      required: [true, "Breed is required"],
     },
     sex: {
       type: String,
-      enum: ['male', 'female'],
+      enum: ["male", "female"],
+      required: true,
+    },
+    size: {
+      type: String,
+      enum: ["big", "average", "small"],
+      required: true,
+    },
+    growth: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
       required: true,
     },
     location: {
       type: String,
-      required: [true, 'Location is required'],
+      required: [true, "Location is required"],
     },
     price: {
       type: Number,
@@ -38,12 +56,39 @@ const noticesSchema = new Schema(
     currency: {
       type: String,
     },
+    passport: {
+      type: String,
+      required: true,
+    },
+    sterilization: {
+      type: String,
+      enum: ["yes", "now"],
+      required: true,
+    },
+    lives: {
+      type: String,
+      enum: ["in street", "shelter", "at volunteers"],
+      required: true,
+    },
     comments: {
       type: String,
-      required: [true, 'Comments is required'],
+      required: [true, "Comments is required"],
     },
     imageUrl: {
       type: String,
+      required: [true, "URL can't be empty"],
+      unique: true,
+    },
+    imageUrl_1: {
+      type: String,
+      unique: true,
+    },
+    imageUrl_2: {
+      type: String,
+      unique: true,
+    },
+    createAt: {
+      type: Date,
     },
   },
   {
@@ -52,6 +97,6 @@ const noticesSchema = new Schema(
   }
 );
 
-const Notices = model('notices', noticesSchema);
+const Notices = model("notices", noticesSchema);
 
 module.exports = Notices;
