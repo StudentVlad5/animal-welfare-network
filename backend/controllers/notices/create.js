e9c9ebd51c43680d16677852378d38e77bc72a5e
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-04-07 22:55:47 +0300

    without multi images

--- backend/controllers/notices/create.js
+++ backend/controllers/notices/create.js
@@ -1,12 +1,11 @@
 const { Notices } = require("../../models");
 
 const createNotices = async (req, res, next) => {
-  const { user, body, params, file, files } = req;
+  const { user, body, params, file } = req;
   const { _id } = user;
   const { category } = params;
   const lower = category.toLowerCase();
-console.log(file)
-console.log(files)
+
   const fullData = !!file
     ? { ...body, category: lower, owner: _id, imageUrl: file.path }
     : { ...body, category: lower, owner: _id };
