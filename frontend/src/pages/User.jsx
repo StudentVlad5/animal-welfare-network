4725891ba13846f6e019f8b61501c724fd04c0f6
JuliaGolban <julia.j.shcherban@gmail.com>
2023-04-08 23:50:52 +0300

    fix user page with footer

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,4 +1,3 @@
-import { Container, Section } from 'components/baseStyles/CommonStyle.styled';
 import { UserComp } from 'components/UserComp/UserComp';
 import { SEO } from 'utils/SEO';
 
@@ -6,19 +5,7 @@ const UserPage = () => {
   return (
     <>
       <SEO title="Profile" description="User profile" />
-      <Section
-        paddingTop="60px"
-        paddingBottom="80px"
-        paddingBottomDesktop="40px"
-      >
-        <Container
-          flexDirectionDesktop="row"
-          justifyContentDesktop="space-between"
-          alignItemsDesktop="flex-start"
-        >
-          <UserComp />
-        </Container>
-      </Section>
+      <UserComp />
     </>
   );
 };
