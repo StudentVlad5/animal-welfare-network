97afe31303323cd3c2b32c876e75c04000d56f1c
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-04-12 16:20:47 +0300

    basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="animal-welfare-network.cyclic.app">
+        <BrowserRouter basename="animal-welfare-network">
           <GlobalStyle />
           <App />
         </BrowserRouter>
