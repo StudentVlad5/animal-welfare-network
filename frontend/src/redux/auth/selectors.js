b94a65929b0d86b7e187e06e9c0ff2fc9d862add
JuliaGolban <julia.j.shcherban@gmail.com>
2023-04-18 21:41:48 +0300

    add ability to delete cards

--- frontend/src/redux/auth/selectors.js
+++ frontend/src/redux/auth/selectors.js
@@ -2,6 +2,6 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user.userName;
 export const selectFavorites = state => state.auth.user.favorites;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
-export const selecId = state => state.auth.user;
+export const selectId = state => state.auth.user._id;
 export const getUser = ({ auth }) => auth.user;
-export const getPermission = ({ auth }) => auth.user.role;
+export const getPermission = ({ auth }) => auth.permission;
