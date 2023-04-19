bc3714ba7c2808466d1d520e3bfe54a0f5c42dd3
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-04-19 21:18:02 +0300

    dep

--- frontend/src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItemSwiper/NoticeCategoryItemSwiper.jsx
+++ frontend/src/components/NoticesComp/NoticesCategoriesList/NoticeCategoryItem/NoticeCategoryItemSwiper/NoticeCategoryItemSwiper.jsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { MdClose } from 'react-icons/md';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { closeModalWindow } from 'hooks/modalWindow';
 import { cleanModal } from 'redux/modal/operation';
 import { modalComponent } from 'redux/modal/selectors';
@@ -10,11 +15,6 @@ import {
   ContainerPositionForCloseModal,
   BackDrop,
 } from './NoticeCategoryItemSwiper.styled';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 export const NoticeCategoryItemSwiper = () => {
   const dispatch = useDispatch();
