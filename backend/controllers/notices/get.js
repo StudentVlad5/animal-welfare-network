a7be03486d68742859227c7be604c88354af21ee
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-04-21 10:41:15 +0300

    turn back

--- backend/controllers/notices/get.js
+++ backend/controllers/notices/get.js
@@ -1,5 +1,5 @@
 const { Notices } = require("../../models");
-const { constructorResponse } = require("../../helpers");
+const { ValidationError, constructorResponse } = require("../../helpers");
 
 const get = async (req, res, next) => {
   try {
@@ -14,17 +14,8 @@ const get = async (req, res, next) => {
     } = req.query;
     const limit = perPage * 1;
     const skip = perPage * (page - 1);
+
     const category = req.params.category;
-
-    // const {
-      // birthday,
-      // typeofpet,
-      // size,
-      // sterilization,
-      // lives,
-      // sex
-    // } = req.body;
-
     let total = await Notices.find({ category }).count();
     let notices = [];
     const constructorData = {
@@ -131,6 +122,103 @@ const get = async (req, res, next) => {
   } catch (error) {
     res.status(400).json({ message: "Invalid search characters" });
   }
+  // const isPagination = req.query.page;
+  // const {
+  //   search,
+  //   isFavorites,
+  //   myAdds,
+  //   findtext,
+  //   page = 1,
+  //   perPage = isPagination ? 20 : 5000,
+  // } = req.query;
+  // const limit = perPage * 1;
+  // const skip = perPage * (page - 1);
+
+  // //   console.log('!!!!!!!!', typeof isFavorites);
+  // const category = req.params.category;
+  // const total = await Notices.find().count();
+  // let notices = [];
+  // const constructorData = {
+  //   pagination: isPagination,
+  //   total,
+  //   perPage,
+  //   // data: notices,
+  //   page,
+  // };
+
+  // if (req.user) {
+  //   const { _id } = req.user;
+  //   if (isFavorites === 'true' && myAdds === 'true') {
+  //     throw new ValidationError(
+  //       'is Favorites and myAdds must not have the same values ​(true)'
+  //     );
+  //   }
+  //   console.log(req.user.favorites[0]);
+  //   if (isFavorites === 'true') {
+  //     if (findtext) {
+  //       console.log(findtext);
+  //       notices = await Notices.find({
+  //         category,
+  //         _id: { $in: req.user.favorites },
+  //         title: { $regex: findtext, $options: 'i' },
+  //       })
+  //         .limit(limit)
+  //         .skip(skip);
+  //       return res
+  //         .status(200)
+  //         .json(constructorResponse(constructorData, notices));
+  //     }
+  //     notices = await Notices.find({
+  //       category,
+  //       _id: { $in: req.user.favorites },
+  //     })
+  //       .limit(limit)
+  //       .skip(skip);
+  //     return res
+  //       .status(200)
+  //       .json(constructorResponse(constructorData, notices));
+  //   }
+
+  //   if (myAdds === 'true') {
+  //     if (findtext) {
+  //       notices = await Notices.find({
+  //         category,
+  //         owner: _id,
+  //         title: { $regex: findtext, $options: 'i' },
+  //       })
+  //         .limit(limit)
+  //         .skip(skip);
+  //       return res
+  //         .status(200)
+  //         .json(constructorResponse(constructorData, notices));
+  //     }
+  //     notices = await Notices.find({ category, owner: _id })
+  //       .limit(limit)
+  //       .skip(skip);
+
+  //     return res
+  //       .status(200)
+  //       .json(constructorResponse(constructorData, notices));
+  //   }
+  // }
+
+  // if (findtext) {
+  //   notices = await Notices.find({
+  //     category: category,
+  //     title: { $regex: findtext, $options: 'i' },
+  //   })
+  //     .limit(limit)
+  //     .skip(skip);
+  //   res.status(200).json(constructorResponse(constructorData, notices));
+  // } else {
+  //   notices = await Notices.find({
+  //     category: { $regex: category, $options: 'i' },
+  //   })
+  //     .limit(limit)
+  //     .skip(skip);
+  //   res.status(200).json(constructorResponse(constructorData, notices));
+  // }
+  // res.status(200).json('1');
 };
 
 module.exports = get;
