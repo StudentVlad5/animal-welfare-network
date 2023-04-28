02429ccbb2717acd87f6dbe02a62128ccb4a06b1
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-04-28 15:48:01 +0300

    // _id: { type: String },

--- backend/models/notices.js
+++ backend/models/notices.js
@@ -3,7 +3,7 @@ const { Schema, model, SchemaTypes } = require("mongoose");
 const noticesSchema = new Schema(
   {
     owner: { type: SchemaTypes.ObjectId, ref: "users" },
-    _id: { type: String },
+    // _id: { type: String },
     category: {
       type: String,
       enum: ["sell", "lost-found", "for-free", "none"],
