5da8beb22327f2358a25184fb48b568a34dd8ed3
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-02 19:58:51 +0300

    search news

--- backend/controllers/news.js
+++ backend/controllers/news.js
@@ -12,29 +12,28 @@ const news = async (req, res, next) => {
   } = req.query;
   const limit = perPage * 1;
   const skip = perPage * (page - 1);
-
-  (function execute() {
-    const options = {
-      url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&facet_fields=ingredients&page=1&q=pet&sort=newest&api-key=${API_KEY}`,
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    };
-    request(options, function (err, res, body) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(body);
-      }
-    });
-  })();
-
   try {
+    (function execute() {
+      const options = {
+        url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&facet_fields=ingredients&page=1&q=pet&sort=newest&api-key=${API_KEY}`,
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      };
+      request(options, function (err, res, body) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(body);
+        }
+      });
+    })();
     // const total = await News.find().count();
-    const total = search
-      ? await News.find({ title: { $regex: search, $options: "i" } }).count()
-      : await News.find().count();
+    // const total = search
+    //   ? await News.find({ title: { $regex: search, $options: "i" } }).count()
+    //   : await News.find().count();
+    const total = await body.response.doc.length;
     const constructorData = {
       pagination: isPagination,
       total,
@@ -44,20 +43,37 @@ const news = async (req, res, next) => {
     };
     if (search) {
       console.log("search: ", search);
-      const news = await News.find({
-        title: { $regex: search, $options: "i" },
-      })
-        .limit(limit)
-        .skip(skip)
-        .sort({ date: -1 });
+      (function execute() {
+        const options = {
+          url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&facet_fields=ingredients&page=1&q=${search}&sort=newest&api-key=${API_KEY}`,
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        };
+        request(options, function (err, res, body) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(body);
+          }
+        });
+      })();
+      // const news = await News.find({
+      //   title: { $regex: search, $options: "i" },
+      // })
+      //   .limit(limit)
+      //   .skip(skip)
+      //   .sort({ date: -1 });
       if (isPagination) {
         return res.status(200).json(constructorResponse(constructorData, news));
       }
       return res.status(200).json(news);
     }
-    console.log("limit: ", limit, "\tskip: ", skip);
-    const news = await News.find().limit(limit).skip(skip).sort({ date: -1 });
-    console.log("total", total);
+    // console.log("limit: ", limit, "\tskip: ", skip);
+    // const news = await News.find().limit(limit).skip(skip).sort({ date: -1 });
+    const news = body;
+    // console.log("total", total);
     // console.log('news: ', news);
     // const constructorData = {
     //   pagination: isPagination,
