ea911d89985b9f7b12cc98fa339a9e95edfa0125
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-03 15:01:39 +0300

    res.status(200).json(body)

--- backend/controllers/news.js
+++ backend/controllers/news.js
@@ -11,48 +11,46 @@ const news = async (req, res, next) => {
     page = 1,
     perPage = isPagination ? 20 : 5000,
   } = req.query;
-  function listnews  ()  {
-    const options = {
+  const options = {
     url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&facet_fields=ingredients&page=1&q=pet&sort=newest&api-key=${API_KEY}`,
     method: "GET",
     headers: {
       Accept: "application/json",
     },
   };
-request(options, function (err, res, body) {
+
+  request(options, function (err, res, body) {
     if (err) {
       console.error(err);
     } else {
-      return body;
+      return res.status(200).json(body);
     }
   });
-  }
-  arrayNews = new Promise(listnews())
-  console.log("arrayNews: ", arrayNews);
-  try {
-    const total = await arrayNews.response.docs.length;
-    const constructorData = {
-      pagination: isPagination,
-      total,
-      perPage,
-      // data: news,
-      page,
-    };
-    if (search) {
-      console.log("search: ", search);
-      const news = JSON.parse(JSON.stringify(arrayNews));
-      if (isPagination) {
-        return res.status(200).json(constructorResponse(constructorData, news));
-      }
-      return res.status(200).json(news);
-    }
 
-    const news = JSON.parse(JSON.stringify(arrayNews));
+  // try {
+  //   const total = await arrayNews.response.docs.length;
+  //   const constructorData = {
+  //     pagination: isPagination,
+  //     total,
+  //     perPage,
+  //     // data: news,
+  //     page,
+  //   };
+  //   if (search) {
+  //     console.log("search: ", search);
+  //     const news = JSON.parse(JSON.stringify(arrayNews));
+  //     if (isPagination) {
+  //       return res.status(200).json(constructorResponse(constructorData, news));
+  //     }
+  //     return res.status(200).json(news);
+  //   }
 
-    res.status(200).json(constructorResponse(constructorData, news));
-  } catch (err) {
-    throw new ValidationError(err.message);
-  }
+  //   const news = JSON.parse(JSON.stringify(arrayNews));
+
+  //   res.status(200).json(constructorResponse(constructorData, news));
+  // } catch (err) {
+  //   throw new ValidationError(err.message);
+  // }
 };
 
 module.exports = news;
