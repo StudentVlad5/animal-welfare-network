6f2556b403b368d568344d4542820bbbba31b2e5
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-03 13:53:28 +0300

    fix

--- backend/controllers/news.js
+++ backend/controllers/news.js
@@ -22,39 +22,38 @@ const news = async (req, res, next) => {
  request(options, function (err, res, body) {
     if (err) {
       console.error(err);
-      return
     } else {
       
       arrayNews.push(body);
       console.log("in request",arrayNews);
-      try {
-        // const total = arrayNews.response.docs.length;
-        const constructorData = {
-          pagination: isPagination,
-          // total,
-          perPage,
-          // data: news,
-          page,
-        };
-        // if (search) {
-        //   console.log("search: ", search);
-        //   const news = JSON.parse(JSON.stringify(arrayNews));
-        //   if (isPagination) {
-        //     return res.status(200).json(constructorResponse(constructorData, news));
-        //   }
-        //   return res.status(200).json(news);
-        // }
-    
-        // const news = JSON.parse(JSON.stringify(arrayNews));
-    
-        return res.status(200).json(constructorResponse(constructorData, arrayNews));
-      } catch (err) {
-        throw new ValidationError(err.message);
-      }
-      return
     }
   });
 
+  console.log("arrayNews: ", arrayNews);
+  try {
+    const total = await arrayNews.response.docs.length;
+    const constructorData = {
+      pagination: isPagination,
+      total,
+      perPage,
+      // data: news,
+      page,
+    };
+    if (search) {
+      console.log("search: ", search);
+      const news = JSON.parse(JSON.stringify(arrayNews));
+      if (isPagination) {
+        return res.status(200).json(constructorResponse(constructorData, news));
+      }
+      return res.status(200).json(news);
+    }
+
+    const news = JSON.parse(JSON.stringify(arrayNews));
+
+    res.status(200).json(constructorResponse(constructorData, news));
+  } catch (err) {
+    throw new ValidationError(err.message);
+  }
 };
 
 module.exports = news;
