31e28188f4570e8429b4c4c8b378a4769aefa01e
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-04 06:58:22 +0300

    const http = require("http");

--- backend/controllers/news.js
+++ backend/controllers/news.js
@@ -1,6 +1,6 @@
 const { ValidationError, constructorResponse } = require("../helpers");
 const { News } = require("../models");
-const request = require("request");
+const http = require("http");
 
 // const execute = async() => {
 //     const options = {
@@ -20,7 +20,7 @@ const request = require("request");
 //   });
 // }
 
-const news = (req, res, next) => {
+const news = async (req, res, next) => {
   const { API_KEY } = process.env;
   const isPagination = req.query.page;
   const {
@@ -28,42 +28,57 @@ const news = (req, res, next) => {
     page = 1,
     perPage = isPagination ? 20 : 5000,
   } = req.query;
-  let arrayNews = "";
-  request(
-    {
-      url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&q=pet&sort=newest&api-key=${API_KEY}`,
+  let data = "";
+  try {
+    const options = {
+      hostname: `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20230401&end_date=20230502&facet=false&q=pet&sort=newest&api-key=${API_KEY}`,
+      path: "/posts",
       method: "GET",
       headers: {
         Accept: "application/json",
       },
-    },
-    function (err, res, body) {
-      if (err) {
-        console.error(err);
-      } else {
-        arrayNews = body;
-        // const total = await arrayNews.response.docs.length;
-        const constructorData = {
-          pagination: isPagination,
-          // total,
-          perPage,
-          // data: news,
-          page,
-        };
-        // if (search) {
-        //   console.log("search: ", search);
-        //   const news = JSON.parse(JSON.stringify(arrayNews));
-        //   if (isPagination) {
-        //     return res.status(200).json(constructorResponse(constructorData, news));
-        //   }
-        //   return res.status(200).json(news);
-        // }
-        const send = constructorResponse(constructorData, arrayNews);
-        return send
-      }
-    }
-  );
-  return res.status(200).json(send);
+    };
+    // Sending the request
+    const req = http
+      .request(options, (res) => {
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        // Ending the response
+        res.on("end", () => {
+          console.log("Body:", JSON.parse(data));
+        });
+      })
+      .on("error", (err) => {
+        console.log("Error: ", err);
+      })
+      .end();
+
+    // const total = await arrayNews.response.docs.length;
+    const news = await JSON.parse(JSON.stringify(arrayNews));
+
+    const constructorData = {
+      pagination: isPagination,
+      // total,
+      perPage,
+      // data: news,
+      page,
+    };
+    // if (search) {
+    //   console.log("search: ", search);
+    //   const news = JSON.parse(JSON.stringify(arrayNews));
+    //   if (isPagination) {
+    //     return res.status(200).json(constructorResponse(constructorData, news));
+    //   }
+    //   return res.status(200).json(news);
+    // }
+    return await res
+      .status(200)
+      .json(constructorResponse(constructorData, news));
+  } catch (err) {
+    throw new ValidationError(err.message);
+  }
 };
 
 module.exports = news;
